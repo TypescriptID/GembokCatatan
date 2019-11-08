04d15ebf2bad0fc3f2c0fc1b2271b1921e7e0605
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-11-08 07:50:08 +0700

    Pembuatan header halaman daftar catatan

--- src/app/loginmodul/loginpage/loginpage.component.ts
+++ src/app/loginmodul/loginpage/loginpage.component.ts
@@ -87,12 +87,10 @@ export class LoginpageComponent implements OnInit {
   }
 
   navigasiHalamanRegistrasi() {
-    console.log('click');
     this.router.navigate(['/masuk-login/registrasi-pengguna'], { replaceUrl: true });
   }
 
   navigasiHalamanCatatan() {
-    console.log('click');
     this.router.navigate(['/catatan'], { replaceUrl: true});
   }
 
